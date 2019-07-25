e177bfa4f57ceba1a5f993dd85edb6f0952b5342
vandai.nguyen <vandai.nguyen@ved.com.vn>
2019-07-25 18:12:17 +0700

    Updates

---
+++ precache-manifest.7c6aef99a464021b51014d51c16d4225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57eec39d677c6459dd5f7e65593ca0cf",
+    "url": "/daitest/index.html"
+  },
+  {
+    "revision": "eb006b967921dcd4e839",
+    "url": "/daitest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/daitest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eb006b967921dcd4e839",
+    "url": "/daitest/static/js/main.faa52dc9.chunk.js"
+  },
+  {
+    "revision": "92b8a9f9ba9ae1d4e0f6",
+    "url": "/daitest/static/js/runtime~main.844afbac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daitest/static/media/logo.5d5d9eef.svg"
+  }
+]);
